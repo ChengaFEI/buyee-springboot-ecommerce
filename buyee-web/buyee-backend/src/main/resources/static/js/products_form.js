8801fd3167b1383f54482cb62c5a476ad38ed923
Cheng Fei <Cheng_a_Fei@outlook.com>
2023-02-11 09:39:14 -0500

    modularize JavaScript codes for products_form.html

---
+++ buyee-web/buyee-backend/src/main/resources/static/js/products_form.js
@@ -0,0 +1,28 @@
+// Brand and categories blanks
+
+dropdownBrands = $("#brand");
+dropdownCategories = $("#category");
+
+// Format the description blank
+
+$(document).ready(function() {
+	$("#shortDescription").richText();
+	$("#fullDescription").richText();
+	dropdownBrands.change(function() {
+		dropdownCategories.empty();
+		readCategories();
+	});
+	readCategories();
+});
+
+// Display categories of the selected brand 
+
+function readCategories() {
+	brandId = dropdownBrands.val();
+	url = brandModuleURL + "/" + brandId + "/categories";
+	$.get(url, function(responseJson) {
+		$.each(responseJson, function(index, category) {
+			$("<option>").val(category.id).text(category.name).appendTo(dropdownCategories);
+		});
+	});
+}

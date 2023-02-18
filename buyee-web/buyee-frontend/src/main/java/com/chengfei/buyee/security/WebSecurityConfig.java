0f30792a54b9bd7b7c67a8b7529f7ae8ba8ebee5
Cheng Fei <Cheng_a_Fei@outlook.com>
2023-02-18 09:45:40 -0500

    revise security package's name in front-end app

--- buyee-web/buyee-frontend/src/main/java/com/chengfei/buyee/security/WebSecurityConfig.java
+++ buyee-web/buyee-frontend/src/main/java/com/chengfei/buyee/security/WebSecurityConfig.java
@@ -1,4 +1,4 @@
-package com.chengfei.buyee.admin.security;
+package com.chengfei.buyee.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;

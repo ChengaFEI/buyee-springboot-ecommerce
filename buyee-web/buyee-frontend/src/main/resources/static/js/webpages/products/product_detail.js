2613b324d7ece274995ee5f314b2c30e5012475f
Cheng Fei <Cheng_a_Fei@outlook.com>
2023-02-18 04:39:33 -0500

    add images carousel to products details page

--- buyee-web/buyee-frontend/src/main/resources/static/js/webpages/products/product_detail.js
+++ buyee-web/buyee-frontend/src/main/resources/static/js/webpages/products/product_detail.js
@@ -3,9 +3,19 @@ $(document).ready(function() {
 	let bigImage = $("#image-big");
 	$(".image-thumbnail").mouseover(function() {
 		let currentImageSrc = $(this).attr("src");
+		let currentImageIndex = $(this).attr("data-index");
 		bigImage.attr("src", currentImageSrc);
+		bigImage.attr("data-index", currentImageIndex);
+	});
+	bigImage.mouseover(function() {
+		bigImage.attr("data-index", 0);
 	});
 	$(".image-thumbnail").mouseout(function() {
 		bigImage.attr("src", mainImagePathURL);
+	});
+	$(".image-thumbnail").on("click", function() {
+		$("#carouselModal").modal("show");
+		imageIndex = parseInt(bigImage.attr("data-index"));
+		$("#carouselIndicators").carousel(imageIndex);
 	});
 });

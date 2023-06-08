de946c230fb008ca20b5389701278da4a7e202b9
Cheng Fei <Cheng_a_Fei@outlook.com>
2023-06-07 23:05:49 -0700

    update README.md, add images and gifs

--- README.md
+++ README.md
@@ -2,22 +2,30 @@
 
 E-Commerce website built with Spring Boot and Amazon S3
 
-## Author
+## 🤗 Author
 
-Cheng Fei
+Cheng Fei (Individual Project)
 
-## Introduction
+## 📋 Introduction
 
-Buyee is an administration system for the online shopping mall. It contains "Users", "Categories", "Brands" modules. Users can login to the system and manage data.
+Buyee is an e-commerce platform with both toB and toC applications. It's a clone of the real-life amazon platform. The application is built with Spring Boot and Amazon S3, and deployed on Heroku.
 
-## Technology Stack
+## ✨ Overview
+
+Frontend:
+![frontend-demo](docs/frontend-demo.gif)
+
+Backend:
+![backend-demo](docs/backend-demo.gif)
+
+## 💻 Technology Stack
 
 Front-end: Thymeleaf (HTML), Bootstrap (CSS), jQuery (Javascript) </br>
 Back-end: Spring Framework (Java) -- Spring Data JPA, Spring Security, Spring RESTful API </br>
 Database: MySQL, Amazon S3 </br>
 Deploy: Heroku
 
-## Authentication
+## 🔒 Authentication
 
 The authentication system is secured with Spring Security framework. Users can only access modules open to their roles.
 
@@ -25,19 +33,45 @@ Users module requires Admin role <br />
 Categories module requires Admin/Editor role <br />
 Brands module requires Admin/Editor role
 
-## Usage
+to be continued...
 
-Please open the root folder buyee-app in an IDE and start the Spring Boot Application in the buyee-web/buyee-backend folder. Then please open the following URL:
+## ✏️ Usage
+
+### Step 1: Database Setup
+
+Set up a local MySQL database schema called `buyee_db` and replace the username and password in the `application.properties` file with your own MySQL username and password.
+
+![database-setup](docs/database-setup.png)
+
+### Step 2: Run the Spring Boot Application
+
+Please open the root folder buyee-app in an IDE and start the backend Spring Boot Application in the `buyee-web/buyee-backend` folder. Then please open the following URL:
 
 ```bash
 open http://localhost:8080/buyee-admin
 ```
 
+or start the frontend application in the `buyee-web/bu` folder. Then please open the following URL:
+
+```bash
+open http://localhost:80/buyee
+```
+
+### Step 3: Login
+
 Then, you are in the login webpage. Please use the following admin account to login.
+
+![login-page](docs/login-page.png)
 
 ```bash
 Username: test_user@test.com
 Password: test2022
 ```
 
+### Step X: Enjoy
+
 Then please enjoy exploring the buyee e-commerce app!
+
+## ☕ Buyee Me A Coffee
+
+[!["Buy Me A Coffee"](https://www.buymeacoffee.com/assets/img/custom_images/orange_img.png)](https://www.buymeacoffee.com/ChengaFEI)
--- buyee-web/buyee-frontend/src/main/java/com/chengfei/buyee/AmazonS3Util.java
+++ buyee-web/buyee-frontend/src/main/java/com/chengfei/buyee/AmazonS3Util.java
@@ -1,4 +1,5 @@
 package com.chengfei.buyee;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
@@ -13,62 +14,67 @@ import software.amazon.awssdk.services.s3.model.ListObjectsRequest;
 import software.amazon.awssdk.services.s3.model.ListObjectsResponse;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 import software.amazon.awssdk.services.s3.model.S3Object;
+
 public class AmazonS3Util {
-    private static final Logger LOGGER = LoggerFactory.getLogger(AmazonS3Util.class);
-    private static final String BUCKET_NAME;
-    static {
-	BUCKET_NAME = System.getenv("AWS_BUCKET_NAME");
-    }
-    // Create Tasks
-    public static void saveFile(String folderName, String fileName, InputStream inputStream) {
-	S3Client client = S3Client.builder().build();
-	PutObjectRequest request = PutObjectRequest.builder().bucket(BUCKET_NAME)
-		.key(folderName + "/" + fileName)
-		.build();
-	try (inputStream) {
-	    int contentLength = inputStream.available();
-	    client.putObject(request, RequestBody.fromInputStream(inputStream, contentLength));
-	} catch (IOException e) {
-	    LOGGER.error("Could not upload file to Amazon S3.", e);
+	private static final Logger LOGGER = LoggerFactory.getLogger(AmazonS3Util.class);
+	private static final String BUCKET_NAME;
+	static {
+		BUCKET_NAME = System.getenv("AWS_BUCKET_NAME");
 	}
-    }
-    // Read Tasks
-    public static List<String> listFolder(String folderName) {
-	S3Client client = S3Client.builder().build();
-	ListObjectsRequest listRequest = ListObjectsRequest.builder()
-		.bucket(BUCKET_NAME).prefix(folderName)
-		.build();
-	ListObjectsResponse response = client.listObjects(listRequest);
-	List<S3Object> contents = response.contents();
-	ListIterator<S3Object> listIterator = contents.listIterator();
-	List<String> listKeys = new ArrayList<>();
-	while (listIterator.hasNext()) {
-	    S3Object object = listIterator.next();
-	    listKeys.add(object.key());
+
+	// Create Tasks
+	public static void saveFile(String folderName, String fileName, InputStream inputStream) {
+		S3Client client = S3Client.builder().build();
+		PutObjectRequest request = PutObjectRequest.builder().bucket(BUCKET_NAME)
+				.key(folderName + "/" + fileName)
+				.build();
+		try (inputStream) {
+			int contentLength = inputStream.available();
+			client.putObject(request, RequestBody.fromInputStream(inputStream, contentLength));
+		} catch (IOException e) {
+			LOGGER.error("Could not upload file to Amazon S3.", e);
+		}
 	}
-	return listKeys;
-    }
-    // Delete Tasks
-    public static void deleteFile(String fileName) {
-	S3Client client = S3Client.builder().build();
-	DeleteObjectRequest request = DeleteObjectRequest.builder()
-		.bucket(BUCKET_NAME).key(fileName)
-		.build();
-	client.deleteObject(request);
-    }
-    public static void deleteFolder(String folderName) {
-	S3Client client = S3Client.builder().build();
-	ListObjectsRequest listRequest = ListObjectsRequest.builder()
-		.bucket(BUCKET_NAME).prefix(folderName)
-		.build();
-	ListIterator<S3Object> listIterator = client.listObjects(listRequest)
-		.contents().listIterator();
-	while (listIterator.hasNext()) {
-	    S3Object object = listIterator.next();
-	    DeleteObjectRequest request = DeleteObjectRequest.builder()
-		    .bucket(BUCKET_NAME).key(object.key())
-		    .build();
-	    client.deleteObject(request);
+
+	// Read Tasks
+	public static List<String> listFolder(String folderName) {
+		S3Client client = S3Client.builder().build();
+		ListObjectsRequest listRequest = ListObjectsRequest.builder()
+				.bucket(BUCKET_NAME).prefix(folderName)
+				.build();
+		ListObjectsResponse response = client.listObjects(listRequest);
+		List<S3Object> contents = response.contents();
+		ListIterator<S3Object> listIterator = contents.listIterator();
+		List<String> listKeys = new ArrayList<>();
+		while (listIterator.hasNext()) {
+			S3Object object = listIterator.next();
+			listKeys.add(object.key());
+		}
+		return listKeys;
 	}
-    }
+
+	// Delete Tasks
+	public static void deleteFile(String fileName) {
+		S3Client client = S3Client.builder().build();
+		DeleteObjectRequest request = DeleteObjectRequest.builder()
+				.bucket(BUCKET_NAME).key(fileName)
+				.build();
+		client.deleteObject(request);
+	}
+
+	public static void deleteFolder(String folderName) {
+		S3Client client = S3Client.builder().build();
+		ListObjectsRequest listRequest = ListObjectsRequest.builder()
+				.bucket(BUCKET_NAME).prefix(folderName)
+				.build();
+		ListIterator<S3Object> listIterator = client.listObjects(listRequest)
+				.contents().listIterator();
+		while (listIterator.hasNext()) {
+			S3Object object = listIterator.next();
+			DeleteObjectRequest request = DeleteObjectRequest.builder()
+					.bucket(BUCKET_NAME).key(object.key())
+					.build();
+			client.deleteObject(request);
+		}
+	}
 }
